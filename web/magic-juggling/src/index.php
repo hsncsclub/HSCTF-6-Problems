54dfa0672e3f3c5ae0f03f1f7b5f58c7154a105e
Daniel Wang <hello@danielwang.dev>
2019-05-15 00:36:37 -0400

    feat(web): add magic-juggling

---
+++ web/magic-juggling/src/index.php
@@ -0,0 +1,18 @@
+<?php
+$flag = file_get_contents("/flag");
+
+if (!isset($_GET["md4"]))
+{
+	highlight_file(__FILE__);
+	die();
+}
+
+if($_GET["md4"] == hash("md4", $_GET["md4"]))
+{
+	echo $flag;
+}
+else
+{
+	echo "bad";
+}
+?>
