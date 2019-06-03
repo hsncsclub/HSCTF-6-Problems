7344e0f2b40e35841662a287ca36729a301190c2
Ptomerty <Ptomerty@users.noreply.github.com>
2019-06-03 06:56:25 -0400

    fix(pwn): update bit/byte dockerfiles and flags

--- pwn/bit/compile.sh
+++ pwn/bit/compile.sh
@@ -4,6 +4,6 @@ mkdir -p bin
 
 
 gcc bit.c -o bit -m32 -no-pie -g0 
-echo 'HSCTF{flippin_pwn_g0d}' > flag
+echo 'hsctf{flippin_pwn_g0d}' > flag
 
 mv flag bit bin/
--- pwn/byte/compile.sh
+++ pwn/byte/compile.sh
@@ -4,6 +4,6 @@ rm -rf bin
 mkdir -p bin
 
 gcc byte.c -o byte -m32 -fPIE -pie -O0 -Wl,-z,relro,-z,now -g0 -fstack-protector
-echo 'HSCTF{l0l-opt1mizati0ns_ar3-disabl3d}' > flag
+echo 'hsctf{l0l-opt1mizati0ns_ar3-disabl3d}' > flag
 
 mv flag byte bin/
