8e3c8ea177f631f11143f012407e36b49ecb4e44
Ptomerty <Ptomerty@users.noreply.github.com>
2019-06-03 07:31:54 -0400

    fix(pwn): recompile pwn

--- pwn/return-to-sender/compile.sh
+++ pwn/return-to-sender/compile.sh
@@ -2,7 +2,7 @@
 
 rm -r bin
 
-gcc return-to-sender.c -o return-to-sender -m32 -no-pie -g0 
+gcc return-to-sender.c -o return-to-sender -m32 -no-pie -g0 -fno-stack-protector
 echo 'hsctf{fedex_dont_fail_me_now}' > flag
 
 mkdir -p bin
