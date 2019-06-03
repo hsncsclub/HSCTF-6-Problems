653fd90712a69e22eae890e6a2c426412fbcb13b
Ptomerty <Ptomerty@users.noreply.github.com>
2019-06-03 08:04:14 -0400

    fix(pwn): re-enable full RELRO for byte

--- pwn/byte/compile.sh
+++ pwn/byte/compile.sh
@@ -3,7 +3,7 @@
 rm -rf bin
 mkdir -p bin
 
-gcc byte.c -o byte -m32 -fPIE -pie -O0 -Wl,-z,relro -g0 -fstack-protector
+gcc byte.c -o byte -m32 -fPIE -pie -O0 -Wl,-z,relro,-z,now -g0 -fstack-protector
 echo 'hsctf{l0l-opt1mizati0ns_ar3-disabl3d}' > flag
 
 mv flag byte bin/
