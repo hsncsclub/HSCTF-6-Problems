4ddb141807896d2e855088afcb38f9285efc19e2
Arinerron <arinesau@gmail.com>
2019-06-03 00:03:02 -0700

    Update bit and upload byte

--- pwn/bit/compile.sh
+++ pwn/bit/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -r bin
+mkdir -p bin
+
 
 gcc bit.c -o bit -m32 -no-pie -g0 
-echo 'hsctf{flippin_pwn_g0d}' > flag
+echo 'HSCTF{flippin_pwn_g0d}' > flag
 
-mkdir -p bin
 mv flag bit bin/
