cd8f43a54e3d46333b2efa88690c0900c821cc2f
comet <comet@archlinux.comet.local>
2019-06-04 21:00:10 -0500

    update tux kitchen dokcer

--- crypto/Tux's Kitchen/files/chall.py
+++ crypto/Tux's Kitchen/files/chall.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python2
+
+import SocketServer
 import random
 
 good_image = """
@@ -70,6 +73,19 @@ def final_baking(food,key):
 	treasure = prepare(treasure)
 	return treasure
 
-print(good_image)
-key = bake_it()
-print(final_baking(flag,key))
+SocketServer.TCPServer.allow_reuse_address = True
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+	def handle(self):
+		self.request.settimeout(15)
+		self.request.sendall(good_image)
+		key = bake_it()
+		self.request.sendall(str(final_baking(flag,key)))
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+	pass
+
+if __name__ == '__main__':
+	server = ThreadedTCPServer(('0.0.0.0', 2000), ThreadedTCPRequestHandler)
+	server.allow_reuse_address = True
+	server.serve_forever()
