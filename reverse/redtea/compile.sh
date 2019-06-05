e0b2dedd7a38236663da27413419d58f7e0f9bc5
Ptomerty <Ptomerty@users.noreply.github.com>
2019-06-04 22:41:41 -0400

    feat(rev): add redtea

--- reverse/redtea/compile.sh
+++ reverse/redtea/compile.sh
@@ -4,6 +4,7 @@ rm -rf bin
 mkdir -p bin
 
 gcc redtea.c -o redtea -Wall -m32 -fPIE -pie -Os -Wl,-z,relro,-z,now -g0 -fstack-protector
-echo 'HSCTF{rob_made_jess_a_cup_of_tea_wink_wink_101}' > flag.txt
+strip redtea
+echo 'hsctf{rob_made_some1_special_a_cup_of_tea_wink_wink_101}' > flag.txt
 
-mv flag.txt redtea bin/
+cp words.txt flag.txt redtea bin/
